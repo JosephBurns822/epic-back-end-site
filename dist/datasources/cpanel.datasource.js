a6e730205742475c70f71d693e91ac54b4925ebf
Altreyas <altreyas@Altreyass-MBP.home>
2021-03-01 20:42:31 -0500

    awuwu

--- dist/datasources/cpanel.datasource.js
+++ dist/datasources/cpanel.datasource.js
@@ -8,11 +8,11 @@ const config = {
     name: 'cpanel',
     connector: 'mysql',
     url: 'mysql://jburns11_burns:Manning010@192.185.188.95/jburns11_epic_site_db',
-    host: '',
-    port: 0,
-    user: '',
-    password: '',
-    database: ''
+    host: '192.185.188.95',
+    port: 3306,
+    user: 'jburns11_burns',
+    password: 'Manning010',
+    database: 'jburns11_epic_site_db'
 };
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
